1ee6110bce4ea535c34a42cfc2e82d013692f859
kevin74Unifei <kevin.vieira@yahoo.com.br>
2019-09-17 10:36:34 -0300

    Construtivo que aceita quebrar restrição

--- otimizacao/funcoes_auxiliares.py
+++ otimizacao/funcoes_auxiliares.py
@@ -8,27 +8,35 @@ def entrada_dados(dados_clientes, dados_plantas):
         n_clientes, n_plantas = header.split()
         n_clientes = int(n_clientes)
         n_plantas = int(n_plantas)
+        custo = [];
 
+        n_linhas_array_clientes = int(n_clientes / 10)
+        n_linhas_array_plantas = int(n_plantas / 10)
         for i in range(0, n_clientes):
-            cliente = data.pop(0)
-            dados_clientes['custo'].append(
-                [int(float(cli)) for cli in cliente.split()])
+            for j in range(0, n_linhas_array_plantas):
+                cliente = data.pop(0)
+                for cli in cliente.split():
+                    custo.append(int(float(cli)))
+            dados_clientes['custo'].append(custo)
+            custo = []
+
             [dados_clientes['posicao'].append(i)]
             [dados_clientes['disponivel'].append(1)]
-        n_linhas_array_clientes = int(n_clientes / n_plantas)
 
         for i in range(0, n_linhas_array_clientes):
             demanda = data.pop(0)
             [dados_clientes['demanda'].append(
                 int(float(dem))) for dem in demanda.split()]
 
+        for i in range(0, n_linhas_array_plantas):
+            [dados_plantas['custo'].append(
+                int(float(cus))) for cus in data.pop(0).split()]
+        for i in range(0, n_linhas_array_plantas):
+            [dados_plantas['capacidade'].append(
+                int(float(cap))) for cap in data.pop(0).split()]
+
         for i in range(0, n_plantas):
             [dados_plantas['posicao'].append(i)]
-
-        dados_plantas['custo'] = [
-            int(float(cus)) for cus in data.pop(0).split()]
-        dados_plantas['capacidade'] = [
-            int(float(cap)) for cap in data.pop(0).split()]
 
 
 def saida_dados(text, tipo):
