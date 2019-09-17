dcbbe0e9ac5e8af34e75e67345f6f4f717e46f0e
rodrigoaasm <rodrigoasmaia@gmail.com>
2019-09-16 23:33:01 -0300

    Agora o refinamento é inteligente o suficiente para saber que ele já fez de tudo que podia para executar

--- otimizacao/refinamentos.py
+++ otimizacao/refinamentos.py
@@ -28,16 +28,17 @@ def busca_menor_alocacao_cliente(solucao_inicial,vec_custos_insta,dados_plantas,
          # se a função objetivo do vizinho for menor
          if(copia_solucao['total'] < solucao_inicial['total']):                     
             solucao_inicial = copia_solucao   
-            ind_menor_custo = i        
+            ind_menor_custo = i  
+              
             if(first): break #se for pelo metodo first fecha agora
-            print("\t\tmenor")
+            
 
    if(ind_menor_custo >= 0):
       vetor_uso_demanda[ind_insta_maior_cliente] -= dados_clientes['demanda'][ind_cliente]
       vetor_uso_demanda[ind_menor_custo] += dados_clientes['demanda'][ind_cliente]
-      solucao_inicial['custo'][ind_cliente]  = np.absolute(solucao_inicial['custo'][ind_cliente])
+      solucao_inicial['flag_uso'] = np.zeros(len(dados_clientes['demanda']))
    else:
-      solucao_inicial['custo'][ind_cliente] *= -1        
+      solucao_inicial['flag_uso'][ind_cliente] = 1        
 
    print("Objetivo refinado: %d" %(solucao_inicial['total']))
    return solucao_inicial
@@ -49,6 +50,14 @@ def calcula_uso_demanda(solucao,dados_plantas,dados_clientes):
       vetor_gasto_demanda[solucao['instalacao'][i]] += dados_clientes['demanda'][i]
    return vetor_gasto_demanda
 
+def busca_maior(solucao_inicial):
+   ind_maior = -1
+   maior_valor = -1
+   for i in range(0,len(solucao_inicial['instalacao'])):
+      if(solucao_inicial['custo'][i] > maior_valor and solucao_inicial['flag_uso'][i] != 1):
+         ind_maior = i
+         maior_valor = solucao_inicial['custo'][i]
+   return ind_maior     
 
 def refina_sem_abrir(solucao_inicial_vect,dados_clientes_vect,dados_plantas_vect,passos,first): #dados_clientes,dados_plantas,match) :
   
@@ -69,8 +78,9 @@ def refina_sem_abrir(solucao_inicial_vect,dados_clientes_vect,dados_plantas_vect
    solucao_inicial = {
     'instalacao': np.array(solucao_inicial_vect['instalacao']),
     'custo': np.array(solucao_inicial_vect['custo']),
+    'flag_uso' : np.zeros(len(dados_clientes['demanda'])),    
     'total': int
-   }
+   }   
 
    solucao_inicial['total'] = calcula_funcao_objetivo(solucao_inicial)
    print("Objetivo Construtiva: %d" %( solucao_inicial['total']))
@@ -82,13 +92,15 @@ def refina_sem_abrir(solucao_inicial_vect,dados_clientes_vect,dados_plantas_vect
    flag_sentido = 1
    for i in range(0,passos):
       #busca maior
-      ind_maior_cliente_alocado = solucao_inicial['custo'].argmax()
-
-      print("Maior escolhido %d em %d" %(solucao_inicial['custo'][ind_maior_cliente_alocado],ind_maior_cliente_alocado))    
-
-      solucao_inicial = busca_menor_alocacao_cliente(solucao_inicial,dados_clientes['custo'][ind_maior_cliente_alocado],
+      ind_maior_cliente_alocado = busca_maior(solucao_inicial)
+      #ind_maior_cliente_alocado2 = solucao_inicial['custo'].argmax()
+      
+      if(ind_maior_cliente_alocado >= 0):
+         print("Maior escolhido %d em %d" %(solucao_inicial['custo'][ind_maior_cliente_alocado],ind_maior_cliente_alocado))    
+         solucao_inicial = busca_menor_alocacao_cliente(solucao_inicial,dados_clientes['custo'][ind_maior_cliente_alocado],
                                                          dados_plantas,dados_clientes,vetor_uso_demanda_insta,
                                                          ind_maior_cliente_alocado,first,flag_sentido)
+      else: break                                                   
       #se execucao busca apenas o primeiro inverte o sentido
       if(first):
          flag_sentido *= -1                                                    
